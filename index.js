97cdc14b015083d0d595a492a0b5bce89ece6d5c
Maria Obaturova <m.obaturova@gmail.com>
2018-11-27 06:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+var height = "74";
 
 
 // Don't worry about this
 module.exports = { name, height
 }
 
+if (name === 'Susan') {
+  return 'The name is correct.'
+} else {
+  return 'Expected the name to be Susan'
+}
